b3f2d92d277d0468d27293deac58d3219470e330
Sam Barker <sbarker@redhat.com>
2024-06-06 14:38:55 +1200

    We don't need to give Jackson an input stream.
    Its more efficient to just give jackson access to the raw bytes.
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- kroxylicious-filters/kroxylicious-record-validation/src/main/java/io/kroxylicious/proxy/filter/schema/validation/bytebuf/JsonSyntaxBytebufValidator.java
+++ kroxylicious-filters/kroxylicious-record-validation/src/main/java/io/kroxylicious/proxy/filter/schema/validation/bytebuf/JsonSyntaxBytebufValidator.java
@@ -6,17 +6,19 @@
 
 package io.kroxylicious.proxy.filter.schema.validation.bytebuf;
 
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 
 import org.apache.kafka.common.record.Record;
-import org.apache.kafka.common.utils.ByteBufferInputStream;
+import org.slf4j.Logger;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.kroxylicious.proxy.filter.schema.validation.Result;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Checks if a Record's value is well-formed JSON, optionally checking if
@@ -25,7 +27,7 @@ import io.kroxylicious.proxy.filter.schema.validation.Result;
  */
 class JsonSyntaxBytebufValidator implements BytebufValidator {
     private final boolean validateObjectKeysUnique;
-
+    private final Logger log = getLogger(JsonSyntaxBytebufValidator.class);
     static final ObjectMapper mapper = new ObjectMapper().enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS);
 
     JsonSyntaxBytebufValidator(boolean validateObjectKeysUnique) {
@@ -33,24 +35,29 @@ class JsonSyntaxBytebufValidator implements BytebufValidator {
     }
 
     @Override
-    public Result validate(ByteBuffer buffer, int size, Record record, boolean isKey) {
+    public Result validate(ByteBuffer buffer, int size, Record kafkaRecord, boolean isKey) {
         if (buffer == null) {
             throw new IllegalArgumentException("buffer is null");
         }
         if (size < 1) {
             throw new IllegalArgumentException("size is less than 1");
         }
-        try (InputStream inputStream = new ByteBufferInputStream(buffer);
-                JsonParser parser = mapper.getFactory().createParser(inputStream)) {
+        final JsonFactory factory = mapper.getFactory();
+        try (JsonParser parser = factory.createParser(buffer.array(), buffer.position(), Math.min(size, buffer.limit()))) {
             if (validateObjectKeysUnique) {
                 parser.enable(JsonParser.Feature.STRICT_DUPLICATE_DETECTION);
             }
-            while (parser.nextToken() != null) {
+
+            var ignored = parser.nextToken();
+            while (ignored != null) {
+                ignored = parser.nextToken();
+                //Iterating all tokens to ensure it parses, however we don't care about the tokens themselves.
             }
             return Result.VALID;
         }
         catch (Exception e) {
             String message = "value was not syntactically correct JSON" + (e.getMessage() != null ? ": " + e.getMessage() : "");
+            log.warn(message, e);
             return new Result(false, message);
         }
     }
